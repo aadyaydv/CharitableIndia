459e2e2be1b8a8866dfbf8225d99dbe42dc5da29
aadyaydv <150153992+aadyaydv@users.noreply.github.com>
2024-09-04 23:32:53 +0530

    Create clg_webd_proj_home.js

---
+++ clg_webd_proj_home.js
@@ -0,0 +1,6 @@
+
+
+// HOMEPAGE
+
+
+var start = alert("This website uses cookies to offer you a better browsing experience. By continuing, you agree to its use. To disable, you can change your browser settings at any time. However this might impair the working of our website.");
